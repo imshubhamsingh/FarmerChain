1938b3c0c4dfe3f8f6ae9eb2c826e02d524193a6
Shubham Singh <imshubhamsingh97@gmail.com>
2017-11-14 08:24:32 +0530

    animated login form

--- src/components/sidebar/Login.js
+++ src/components/sidebar/Login.js
@@ -2,22 +2,41 @@ import React, { Component } from 'react'
 import './login.css'
 
 class Login extends Component{
+    state = {
+        username: '',
+        password: '',
+        login: false
+    }
+    login = (event) => {
+        event.preventDefault();
+        console.log(this.state)
+        this.setState({login:true})
+        setTimeout(this.props.onLogin,1000);
+    };
+    handleChange = (event) => {
+        if(event.target.name ==='password'){
+            this.setState({password: event.target.value});
+        }else if(event.target.name ==='username'){
+            this.setState({username: event.target.value});
+        }
+        
+      }
     render(){
         return(
-            <div className="form">
+            <div className={"form "+(this.state.login?"remove-form":'')}>
                 <div className="form-panel one">
                     <div className="form-header">
                         <h1>Account Login</h1>
                     </div>
                     <div className="form-content">
-                        <form>
+                        <form onSubmit={this.login}>
                             <div className="form-group">
                                 <label htmlFor="username">Username</label>
-                                <input type="text" id="username" name="username" required="required"/>
+                                <input type="text" id="username" name="username" required="required" value={this.state.username} onChange={this.handleChange}/>
                             </div>
                             <div className="form-group">
                                 <label htmlFor="password">Password</label>
-                                <input type="password" id="password" name="password" required="required"/>
+                                <input type="password" id="password" name="password" required="required" value={this.state.password} onChange={this.handleChange}/>
                             </div>
                             <div className="form-group">
                                 <label className="form-remember">
--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -6,8 +6,11 @@ class Sidebar extends Component{
     state = {
         login: false
     }
+    dashboard =  () =>{
+        this.setState({login:true})
+    }
     render(){
-        const current = !this.state.login? <Login/>: '' 
+        const current = !this.state.login? <Login onLogin={this.dashboard}/>: '' 
         return(
             <div className={'sidebar box ' + (!this.state.login? 'login-sidebar': '' )}>
                 {current}             
