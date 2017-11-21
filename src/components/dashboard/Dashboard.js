3e67180b8b857c85c203d11cac266c4ad58787be
Shubham Singh <imshubhamsingh97@gmail.com>
2017-11-21 19:13:54 +0530

    added cards to dashboard

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -1,10 +1,21 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import './dashboard.css';
 
 class Dashboard extends Component{
     render(){
         return(        
             <div className='dashboard box'>
-                   
+                <ul className="card-list">
+                    <li className="card-list-item">
+                        <div className="card"></div>
+                    </li>   
+                    <li className="card-list-item">
+                        <div className="card"></div>
+                    </li>  
+                    <li className="card-list-item">
+                        <div className="card"></div>
+                    </li>                                
+                </ul>                
             </div>                              
         )
     }
