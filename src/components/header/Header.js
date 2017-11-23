b6df9f8935eb23c4d63b94fb350f718f1c678aea
Shubham Singh <imshubhamsingh97@gmail.com>
2017-11-23 22:55:28 +0530

    added setting notification and back btn icons

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,11 +1,24 @@
 import React, { Component } from 'react'
+import 'font-awesome/css/font-awesome.min.css'
+import history from "../../history";
 
 
 class Header extends Component{
+    showback = () => {
+        console.log(history.location);
+        if(history.location.pathname !=='/') return <i onClick={history.goBack} className="fa fa-arrow-left" aria-hidden="true" style={{position: 'absolute',left:'37px',fontSize:'20px',color: '#313041'}}></i>
+
+    }
+
     render(){
         return(
             <div>
-                <div className='header box'>Dashboard</div>                           
+                <div className='header box'>
+                    {this.showback()}
+                    Dashboard
+                    <i className="fa fa-cog" aria-hidden="true" style={{position: 'absolute',right:'77px',fontSize:'20px'}}></i>
+                    <i className="fa fa-bell" aria-hidden="true" style={{position: 'absolute',right:'37px',fontSize:'20px'}}></i>
+                </div>
             </div>                     
         )
     }
