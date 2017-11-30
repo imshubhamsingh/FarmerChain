1728b1ba1ba2b4ab5b7c513d88e2df58fdc9cfe2
Shubham Singh <imshubhamsingh97@gmail.com>
2017-11-30 16:50:08 +0530

    added webpack config file

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const webpack = require('webpack');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+
+module.exports = {
+    entry: ['./src/index.js','./scss/style.scss'],
+    output: {
+        path: __dirname+'/public',
+        filename:'bundle.js'
+    },
+    module:{
+        loaders:[
+            {
+                test: /\.js$/,
+                loader: 'babel-loader'
+            },
+            {
+                test: /\.css$/,
+                loader: 'style-loader!css-loader'
+            },
+            {
+                test: /\.json$/,
+                loader: 'json-loader'
+            },
+            {
+                test: /\.scss$/,
+                loader:ExtractTextPlugin.extract('css-loader!sass-loader')
+            }
+        ]
+    },
+    plugins: [
+        new webpack.ProvidePlugin({
+            $: 'jquery',
+            jQuery: 'jquery',
+            'window.jQuery': 'jquery'
+        }),
+        new webpack.DefinePlugin({
+            BASE_URL: 'http://localhost:3030/'
+        }),
+        new ExtractTextPlugin({ // define where to save the file
+            filename: './bundle.css',
+            allChunks: true,
+        })
+    ]
+};
