363ccf34ae57c00fad406898269144a6225e4e0a
Shubham Singh <imshubhamsingh97@gmail.com>
2017-12-01 12:32:55 +0530

    back button fix

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -8,9 +8,9 @@ class Header extends Component{
     render(){
         return(
                 <div className='header box'>
-		            <span onClick={this.props.showSidebar} className="menu-toggle"><i className="fa fa-bars" aria-hidden="true"></i></span>
+		            <span onClick={this.props.showSidebar} className="menu-toggle"><i className="fa fa-bars" aria-hidden="true" style={{position: 'absolute',left:'37px',fontSize:'20px',color: '#313041'}}></i></span>
                     <Link to="/">
-                        <i className="fa fa-arrow-left" aria-hidden="true" style={{position: 'absolute',left:'37px',fontSize:'20px',color: '#313041'}}></i>
+                        <i className="fa fa-arrow-left" aria-hidden="true" style={{position: 'absolute',right: '118px',fontSize:'20px',color: '#313041'}}></i>
                     </Link>
                     {this.props.header}
                     <i className="fa fa-cog" aria-hidden="true" style={{position: 'absolute',right:'77px',fontSize:'20px'}}></i>
