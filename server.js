421e3af2ce1393f11bd6dd533e84e4cb0a5496dc
Shubham Singh <imshubhamsingh97@gmail.com>
2017-12-01 21:32:10 +0530

    project build testing

--- server.js
+++ server.js
@@ -15,4 +15,4 @@ app.get('*', (req, res) => {
 const port = process.env.PORT || 5000;
 app.listen(port);
 
-console.log(`Password generator listening on ${port}`);
+console.log(`Project running on ${port}`);
