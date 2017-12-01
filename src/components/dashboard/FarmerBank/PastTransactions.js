d490870db4c683f852210bfa8da83983eb8baf83
Shubham Singh <imshubhamsingh97@gmail.com>
2017-12-02 00:00:09 +0530

    fix UI/UX related bugs

--- src/components/dashboard/FarmerBank/PastTransactions.js
+++ src/components/dashboard/FarmerBank/PastTransactions.js
@@ -8,7 +8,7 @@ class PastTransactions extends Component {
         return(
             <li className="transaction-list">
                 <div className="info">
-                    {this.props.transaction.from.displayName} ⮕ {this.props.transaction.to.displayName}<br/>
+                    {this.props.transaction.from.displayName} ⇒ {this.props.transaction.to.displayName}<br/>
                     amount: ₹{this.props.transaction.info.amount}<br/>
                     transaction ID: {`0x${this.props.transaction.id}`}<br/>
                 </div>
