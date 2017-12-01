a9663617786679ca4aa6b665cf39452253aed741
Shubham Singh <imshubhamsingh97@gmail.com>
2017-12-01 12:58:35 +0530

    removed babel-plugins

--- config.js
+++ config.js
@@ -3,8 +3,8 @@ const env = process.env;
 export const nodeEnv = env.NODE_ENV || 'developement';
 
 export default {
-    port : env.PORT || '3030',
-    host: env.HOST || '0.0.0.0',
+    port : env.PORT || '3032',
+    host: env.HOST || '127.0.0.1',
     get serverUrl(){
         return `http://${this.host}:${this.port}`;
     }
