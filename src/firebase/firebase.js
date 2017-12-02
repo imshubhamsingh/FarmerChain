08388789c69345c9450a7cc127d398a454ac94e5
Shubham Singh <imshubhamsingh97@gmail.com>
2017-12-02 18:08:03 +0530

    build

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,12 +1,12 @@
 import * as firebase from 'firebase'
 
 const config = {
-    apiKey: "AIzaSyCNlEBXTBdIG5N31D2bl5FlFSzjT1DiBio",
-    authDomain: "digitalocean-1a580.firebaseapp.com",
-    databaseURL: "https://digitalocean-1a580.firebaseio.com",
-    projectId: "digitalocean-1a580",
-    storageBucket: "digitalocean-1a580.appspot.com",
-    messagingSenderId: "104470064238"
+    apiKey: "AIzaSyCHfvinZCTzaHbUedTf68YaRBWOBEufs3w",
+    authDomain: "reactjsfin.firebaseapp.com",
+    databaseURL: "https://reactjsfin.firebaseio.com",
+    projectId: "reactjsfin",
+    storageBucket: "reactjsfin.appspot.com",
+    messagingSenderId: "435192790797"
 };
 
 export const firebaseApp = firebase.initializeApp(config);
