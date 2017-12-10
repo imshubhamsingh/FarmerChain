d6fa17246b3f6fc6cf182321b4713a357bdf8c04
Shubham Singh <imshubhamsingh97@gmail.com>
2017-12-10 11:19:16 +0530

    fixed minor bugs

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,26 +1,34 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import {connect} from 'react-redux';
-import './header.css'
+import { connect } from 'react-redux';
+import './header.css';
 
-
-class Header extends Component{
-
-    render(){
-        return(
-            <div className='header box'>
-                <span onClick={this.props.showSidebar} className="menu-toggle"><i className="fa fa-bars" aria-hidden="true" style={{position: 'absolute',left:'37px',fontSize:'20px',color: '#313041'}}></i></span>
-                <Link to="/">
-                    <i className="fa fa-arrow-left" aria-hidden="true" style={{position: 'absolute',right: '24px',fontSize:'20px',color: '#313041'}}></i>
-                </Link>
-                {this.props.header}
-            </div>
-        );
-    }
+class Header extends Component {
+	render() {
+		return (
+			<div className="header box">
+				<span onClick={this.props.showSidebar} className="menu-toggle">
+					<i
+						className="fa fa-bars"
+						aria-hidden="true"
+						style={{ position: 'absolute', left: '37px', fontSize: '20px', color: '#313041' }}
+					/>
+				</span>
+				<Link to="/" style={{ display: this.props.header === 'Home' ? 'none' : '' }}>
+					<i
+						className="fa fa-arrow-left"
+						aria-hidden="true"
+						style={{ position: 'absolute', right: '24px', fontSize: '20px', color: '#313041' }}
+					/>
+				</Link>
+				{this.props.header}
+			</div>
+		);
+	}
 }
 
 function mapStateToProps(state) {
-    return { header: state.header };
+	return { header: state.header };
 }
 
-export default connect(mapStateToProps,null)(Header);
+export default connect(mapStateToProps, null)(Header);
