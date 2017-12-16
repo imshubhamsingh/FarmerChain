950bc802a560e47fec9e4da538213e472b131dbc
Shubham Singh <imshubhamsingh97@gmail.com>
2017-12-16 09:48:31 +0530

    added .env

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -3,30 +3,49 @@ import Services from './Services/services';
 import PoolFarm from './PoolFarming/PoolFarm';
 import CartFarm from './CartFarm/CartFarm';
 import FarmerBank from './FarmerBank/FarmerBank';
-import NotFound from './404/404'
-import { Route,Switch, withRouter} from 'react-router-dom';
-import {connect} from 'react-redux';
-import './dashboard.css'
-
+import NotFound from './404/404';
+import { Route, Switch, withRouter } from 'react-router-dom';
+import { connect } from 'react-redux';
+import './dashboard.css';
 
 import PageShell from './PageShell/PageShell';
 
-class Dashboard extends Component{
-    render(){
-        return(
-            <div className="dashboard box">
-                <div className="dashboard-bg" />
-                <Switch>
-                    <Route path="/" exact changeHeader={this.props.changeHeader} component={PageShell(Services, this.props.web3)} />
-                    <Route path="/poolfarm" changeHeader={this.props.changeHeader} component={PageShell(PoolFarm, this.props.web3)} />
-                    <Route path="/cartfarm" changeHeader={this.props.changeHeader} component={PageShell(CartFarm, this.props.web3)} />
-                    <Route path="/farmerBank" changeHeader={this.props.changeHeader} component={PageShell(FarmerBank, this.props.web3,this.props.contractInstance)} />
-                    <Route path="*" component={PageShell(NotFound)} />
-                </Switch>
-            </div>
-
-        );
-    }
+class Dashboard extends Component {
+	render() {
+		return (
+			<div className="dashboard box">
+				<div className="dashboard-bg" />
+				<Switch>
+					<Route
+						path="/"
+						exact
+						changeHeader={this.props.changeHeader}
+						component={PageShell(Services, this.props.web3)}
+					/>
+					<Route
+						path="/poolfarm"
+						changeHeader={this.props.changeHeader}
+						component={PageShell(PoolFarm, this.props.web3)}
+					/>
+					<Route
+						path="/cartfarm"
+						changeHeader={this.props.changeHeader}
+						component={PageShell(CartFarm, this.props.web3)}
+					/>
+					<Route
+						path="/farmerBank"
+						changeHeader={this.props.changeHeader}
+						component={PageShell(FarmerBank, this.props.web3, this.props.contractInstance)}
+					/>
+					<Route
+						path="*"
+						changeHeader={this.props.changeHeader}
+						component={PageShell(Services, this.props.web3)}
+					/>
+				</Switch>
+			</div>
+		);
+	}
 }
 
 export default withRouter(connect()(Dashboard));
